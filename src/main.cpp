05a62cb757f19b1d5a2fe292bc2138612cb7ad5c
HaidenKJ <haiden.kaymenjoseph@gmail.com>
2025-09-27 15:07:25 -0400

    All the triangles are there, they do what they are supposed to, but for some reason the rainbox triangle has moved
    My first glance at the code working genuinly made my brain lag and I dont know why
    
    anyways this assaignment should be over after a few tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <glad/glad.h>
 #include <iostream>
 #include <chrono> // That is a cool name, I like it
+#include <cmath>
 
 int main()
 {
@@ -39,7 +40,6 @@ int main()
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    // unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
@@ -48,7 +48,7 @@ int main()
         //  position.x, position.y, position.z,   r,   g,   b
          0.3f,  0.1f, 0.0f,   1.0f, 0.0f, 0.0f, // top - red
          0.2f, -0.1f, 0.0f,   0.0f, 1.0f, 0.0f, // bottom left - greeen
-         0.4f, -0.1f, 0.0f,   0.0f, 0.0f, 1.0f  // bottom righ - blue
+         0.4f, -0.1f, 0.0f,   0.0f, 0.0f, 1.0f  // bottom right - blue
     };
 
     unsigned int rainbowVAO, rainbowVBO;
@@ -59,11 +59,9 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, rainbowVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(rainbowVertices), rainbowVertices, GL_STATIC_DRAW);
 
-    // position attribute (location = 0)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // color attribute (location = 1)
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
@@ -73,9 +71,9 @@ int main()
     // A triangle who’s colour changes over time (using uniforms).
     float ColorChangingVertices[] = {
         //  position.x, position.y, position.z,   r,   g,   b
-         -0.3f,  0.1f, 0.0f,   1.0f, 0.0f, 0.0f, // top - red
-         -0.2f, -0.1f, 0.0f,   0.0f, 1.0f, 0.0f, // bottom left - greeen
-         -0.4f, -0.1f, 0.0f,   0.0f, 0.0f, 1.0f  // bottom righ - blue
+         -0.3f,  0.4f, 0.0f,   1.0f, 0.0f, 0.0f, // top - red (shifted above white triangle)
+         -0.2f,  0.2f, 0.0f,   0.0f, 1.0f, 0.0f, // bottom left - green
+         -0.4f,  0.2f, 0.0f,   0.0f, 0.0f, 1.0f  // bottom right - blue
     };
 
     unsigned int CCVAO, CCVBO;
@@ -86,11 +84,9 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, CCVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(ColorChangingVertices), ColorChangingVertices, GL_STATIC_DRAW);
 
-    // position attribute (location = 0)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // color attribute (location = 1)
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
@@ -100,9 +96,9 @@ int main()
     //  A triangle that translates back and forth from x = 1 to x = -1.
     float TranslatingVertices[] = {
         //  position.x, position.y, position.z,   r,   g,   b
-     0.0f, -0.2f, 0.0f,  1.0f, 1.0f, 0.0f, // top
-    -0.1f, -0.4f, 0.0f,  1.0f, 1.0f, 0.0f, // bottom left
-     0.1f, -0.4f, 0.0f,  1.0f, 1.0f, 1.0f  // bottom right
+         0.0f, -0.2f, 0.0f,  2.0f, 2.0f, 0.0f, // top
+        -0.1f, -0.4f, 0.0f,  2.0f, 2.0f, 0.0f, // bottom left
+         0.1f, -0.4f, 0.0f,  2.0f, 2.0f, 1.0f  // bottom right
     };
 
     unsigned int movingVAO, movingVBO;
@@ -113,11 +109,9 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, movingVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(TranslatingVertices), TranslatingVertices, GL_STATIC_DRAW);
 
-    // position
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // color
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
@@ -138,7 +132,8 @@ int main()
         gl_Position = vec4(aPos, 1.0);
         vertexColor = aColor;
     }
-)";
+    )";
+
     const char* fragmentShaderSource = R"(
     #version 330 core
     in vec3 vertexColor;
@@ -148,7 +143,7 @@ int main()
     {
         FragColor = vec4(vertexColor, 1.0);
     }
-)";
+    )";
 
     const char* fragmentShaderSourceCHANGING = R"(
     #version 330 core
@@ -167,8 +162,9 @@ int main()
 
         FragColor = vec4(animatedColor, 1.0);
     }
-)";
+    )";
 
+    // Vertex shader for moving triangle
     const char* vertexShaderSourceMOVING = R"(
     #version 330 core
     layout(location = 0) in vec3 aPos;
@@ -183,29 +179,54 @@ int main()
         gl_Position = vec4(aPos.xy + uTranslation, aPos.z, 1.0);
         vertexColor = aColor;
     }
-)";
+    )";
 
-    // Compile Vertex Shader
+    // Vertex shader for rotating triangle
+    const char* vertexShaderSourceROTATING = R"(
+    #version 330 core
+    layout(location = 0) in vec3 aPos;
+    layout(location = 1) in vec3 aColor;
+
+    out vec3 vertexColor;
+
+    uniform float uTime;
+
+    void main()
+    {
+        float angle = uTime;
+        float cosA = cos(angle);
+        float sinA = sin(angle);
+
+        // Rotate around center of triangle
+        vec2 center = vec2( -0.3f + 0.1f, 0.2f + 0.1f ); // approximate center
+        vec2 pos = aPos.xy - center;
+        vec2 rotatedPos;
+        rotatedPos.x = pos.x * cosA - pos.y * sinA;
+        rotatedPos.y = pos.x * sinA + pos.y * cosA;
+        rotatedPos += center;
+
+        gl_Position = vec4(rotatedPos, aPos.z, 1.0);
+        vertexColor = aColor;
+    }
+    )";
+
+    // Compile shaders and create programs (same as before)
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
 
-    // Compile Fragment Shader
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
 
-    // Create Shader Program
     unsigned int shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
-    // Clean up shaders
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // Ones below are for the animated triangle
     unsigned int vertexShaderAnimated = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShaderAnimated, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShaderAnimated);
@@ -221,7 +242,6 @@ int main()
 
     GLint timeLocAnimated = glGetUniformLocation(shaderProgramAnimated, "uTime");
 
-    // Ones below are for the left-right moving triangle
     unsigned int vertexShaderMoving = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShaderMoving, 1, &vertexShaderSourceMOVING, NULL);
     glCompileShader(vertexShaderMoving);
@@ -235,12 +255,54 @@ int main()
     glAttachShader(movingShaderProgram, fragmentShaderMoving);
     glLinkProgram(movingShaderProgram);
 
-    GLint translationLoc = glGetUniformLocation(movingShaderProgram, "uTranslation"); // this is incredibly scuffed.
+    GLint translationLoc = glGetUniformLocation(movingShaderProgram, "uTranslation");
+
+    // Rotating triangle shaders
+    unsigned int vertexShaderRotating = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShaderRotating, 1, &vertexShaderSourceROTATING, NULL);
+    glCompileShader(vertexShaderRotating);
+
+    unsigned int fragmentShaderRotating = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShaderRotating, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShaderRotating);
+
+    unsigned int rotatingShaderProgram = glCreateProgram();
+    glAttachShader(rotatingShaderProgram, vertexShaderRotating);
+    glAttachShader(rotatingShaderProgram, fragmentShaderRotating);
+    glLinkProgram(rotatingShaderProgram);
+
+    GLint rotationTimeLoc = glGetUniformLocation(rotatingShaderProgram, "uTime");
 
     glDeleteShader(vertexShaderMoving);
     glDeleteShader(fragmentShaderMoving);
     glDeleteShader(vertexShaderAnimated);
     glDeleteShader(fragmentShaderAnimated);
+    glDeleteShader(vertexShaderRotating);
+    glDeleteShader(fragmentShaderRotating);
+
+    // Rotating triangle vertex array
+    float RotatingVertices[] = {
+        // position.x, position.y, position.z,   r,   g,   b
+         -0.3f,  0.4f, 0.0f,   1.0f, 0.0f, 1.0f, // top - magenta
+         -0.2f,  0.2f, 0.0f,   0.0f, 1.0f, 1.0f, // bottom left - cyan
+         -0.4f,  0.2f, 0.0f,   1.0f, 1.0f, 0.0f  // bottom right - yellow
+    };
+
+    unsigned int rotatingVAO, rotatingVBO;
+    glGenVertexArrays(1, &rotatingVAO);
+    glGenBuffers(1, &rotatingVBO);
+
+    glBindVertexArray(rotatingVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, rotatingVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(RotatingVertices), RotatingVertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     // Main render loop
     while (!WindowShouldClose())
@@ -248,8 +310,8 @@ int main()
         auto now = std::chrono::high_resolution_clock::now();
         float tt = std::chrono::duration<float>(now - startTime).count();
 
-        float speed = 1.0f; // speed of translation
-        float xOffset = sin(tt * speed); // oscillates between -1 and 1
+        float speed = 1.0f;
+        float xOffset = sin(tt * speed);
 
         // Background color
         float r = 239.0f / 255.0f;
@@ -269,8 +331,8 @@ int main()
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // Draw moving triangle
-        glUseProgram(movingShaderProgram); // MUST use moving shader
-        glUniform2f(translationLoc, xOffset, 0.0f); // yOffset = 0 for horizontal movement
+        glUseProgram(movingShaderProgram);
+        glUniform2f(translationLoc, xOffset, 0.0f);
         glBindVertexArray(movingVAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -280,25 +342,31 @@ int main()
         glBindVertexArray(CCVAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
+        // Draw rotating triangle
+        glUseProgram(rotatingShaderProgram);
+        glUniform1f(rotationTimeLoc, tt);
+        glBindVertexArray(rotatingVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         Loop();
     }
 
     // Cleanup
     glDeleteVertexArrays(1, &whiteVAO);
     glDeleteBuffers(1, &whiteVBO);
-
     glDeleteVertexArrays(1, &rainbowVAO);
     glDeleteBuffers(1, &rainbowVBO);
-
     glDeleteVertexArrays(1, &CCVAO);
     glDeleteBuffers(1, &CCVBO);
-
     glDeleteVertexArrays(1, &movingVAO);
     glDeleteBuffers(1, &movingVBO);
+    glDeleteVertexArrays(1, &rotatingVAO);
+    glDeleteBuffers(1, &rotatingVBO);
 
     glDeleteProgram(shaderProgram);
     glDeleteProgram(shaderProgramAnimated);
     glDeleteProgram(movingShaderProgram);
+    glDeleteProgram(rotatingShaderProgram);
 
     DestroyWindow();
     return 0;
